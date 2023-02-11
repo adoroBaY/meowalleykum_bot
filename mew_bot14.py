abb61464da378c84ad0fd93cab2e574970a4c409
adoroBaY <113442186+adoroBaY@users.noreply.github.com>
2023-02-11 11:52:50 +0200

    Update mew_bot14.py

--- mew_bot14.py
+++ mew_bot14.py
@@ -1,15 +1,14 @@
 import logging
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
+import requests
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
-from aiogram.utils import executor
 
-bot = Bot('5489514849:AAFZjTao3YlNhpSOk0Q-hXwEE5XclxQwngg')
+API_TOKEN = '5489514849:AAH2BMUFj7oU_Hp8uNoi5QxzgK2skIPSbD8'
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-
-button1 = KeyboardButton('Yeeesss, show it to me!!! ')
+button1 = KeyboardButton('Yeeesss!!!')
 button2 = KeyboardButton('No!')
-keyboard = ReplyKeyboardMarkup(resize_keyboard=True).row('Yeeesss, show it to me!!!', 'No!')
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[[button1, button2]])
 
 
 @dp.message_handler(commands=['start'])
@@ -26,9 +25,26 @@ async def welcome(message: types.Message):
 async def answer(message: types.Message):
     if message.text == 'No!':
         await bot.send_message(message.chat.id, 'Go away!')
-    elif message.text == 'Yeeesss, show it to me!!!':
+        with open('C:/brother.jpg', 'rb') as bro:
+            bro = bro.read()
+            await bot.send_photo(message.chat.id, bro)
+    elif message.text == 'Yeeesss!!!':
         await bot.send_message(message.chat.id, 'I`ll show you something')
 
+        response = requests.get("https://api.thecatapi.com/v1/images/search?format=json")
+        data = response.json()
+        image_url = data[0]["url"]
+
+        with open("cat.jpg", "wb") as f:
+            f.write(requests.get(image_url).content)
+
+            await bot.send_photo(chat_id=message.chat.id, photo=open('cat.jpg', 'rb'))
+            await bot.send_message(chat_id=message.chat.id, text="Do you want more kitties?")
+
+            if message.text == 'MORE!':
+                await bot.send_message(message.chat.id, 'Ok!')
+                await bot.send_photo(chat_id=message.chat.id, photo=open('cat.jpg', 'rb'))
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+
